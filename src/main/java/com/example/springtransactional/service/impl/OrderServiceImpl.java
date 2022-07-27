311f3f39bd611e1e26a42b6d5fe05cdbd9178b31
Leandro Barros <leandroedbarros@gmail.com>
2022-07-27 19:53:54 -0300

    Bugfix: Adding bean service in orderServiceImpl

--- src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.springtransactional.service.impl;
 
 import com.example.springtransactional.dto.OrderDto;
 import com.example.springtransactional.service.OrderService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderServiceImpl implements OrderService {
 
     @Override
