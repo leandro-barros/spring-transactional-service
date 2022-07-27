5d7bef83e863f3d634f46cb45af30970f0137f6b
Leandro Barros <leandroedbarros@gmail.com>
2022-07-27 19:58:57 -0300

    Feature: Implementing method to save order

--- src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
@@ -1,15 +1,25 @@
 package com.example.springtransactional.service.impl;
 
 import com.example.springtransactional.dto.OrderDto;
+import com.example.springtransactional.model.OrderModel;
+import com.example.springtransactional.repository.OrderRepository;
 import com.example.springtransactional.service.OrderService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    private OrderRepository orderRepository;
+
     @Override
     public OrderDto save(OrderDto orderDto) {
-        return null;
+        var orderModel = new OrderModel();
+        BeanUtils.copyProperties(orderDto, orderModel);
+        orderRepository.save(orderModel);
+
+        orderDto.setId(orderModel.getId());
+        return orderDto;
     }
 
 }
