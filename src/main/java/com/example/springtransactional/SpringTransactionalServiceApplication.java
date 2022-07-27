ac828052c2975851b60a637351730b54d0e33a6f
Leandro Barros <leandroedbarros@gmail.com>
2022-07-27 15:37:09 -0300

    First commit

---
+++ src/main/java/com/example/springtransactional/SpringTransactionalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtransactional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtransactional/SpringTransactionalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtransactional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
