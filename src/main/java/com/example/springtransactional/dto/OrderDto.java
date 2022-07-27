f9f163f5ebcf5b3384b82a2a0609a8734196cfcc
Leandro Barros <leandroedbarros@gmail.com>
2022-07-27 19:46:27 -0300

    Feature: implementing DTO in the order entity

---
+++ src/main/java/com/example/springtransactional/dto/OrderDto.java
@@ -0,0 +1,22 @@
+package com.example.springtransactional.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class OrderDto {
+
+    private UUID id;
+
+    private Long number;
+
+    private LocalDate dataOrder;
+
+    private BigDecimal value;
+
+}
