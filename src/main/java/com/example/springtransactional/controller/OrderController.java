3273b4989217cc1f0daed57e87ab94f6d95b1174
Leandro Barros <leandroedbarros@gmail.com>
2022-07-31 15:39:48 -0300

    Bugfix: Correction mapping method save

--- src/main/java/com/example/springtransactional/controller/OrderController.java
+++ src/main/java/com/example/springtransactional/controller/OrderController.java
@@ -2,17 +2,20 @@ package com.example.springtransactional.controller;
 
 import com.example.springtransactional.dto.OrderDto;
 import com.example.springtransactional.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/orders")
+@RequestMapping("v1/orders")
 @RestController
 public class OrderController {
 
     private OrderService orderService;
 
+    @PostMapping
     public ResponseEntity<OrderDto> save(@RequestBody OrderDto orderDto) {
         return ResponseEntity.ok().body(orderService.save(orderDto));
     }
