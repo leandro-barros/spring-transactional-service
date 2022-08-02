fe97ce32cd4977df2d0e376f05814ea25a4e3ce1
Leandro Barros <leandroedbarros@gmail.com>
2022-08-02 14:50:55 -0300

    Feature: Simulating transaction

--- src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
+++ src/main/java/com/example/springtransactional/service/impl/OrderServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.springtransactional.service.OrderService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -14,12 +16,15 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
     @Override
     public OrderDto save(OrderDto orderDto) {
         var orderModel = new OrderModel();
         BeanUtils.copyProperties(orderDto, orderModel);
         orderRepository.save(orderModel);
-
+        if (true) {
+            throw new RuntimeException();
+        }
         orderDto.setId(orderModel.getId());
         return orderDto;
     }
